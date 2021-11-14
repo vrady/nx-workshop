2b9a8701ed06911b5209b479b2b599565afe1356
vrady <wiggledima@gmail.com>
2021-11-14 20:50:03 +0200

    add lazy load routing library

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -10,9 +10,20 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (module) => module.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
     MatCardModule,
-    StoreUiSharedModule
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+
+import { GameDetailComponent } from './game-detail/game-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', pathMatch: 'full', component: GameDetailComponent }
+    ]),
+    MatCardModule
+  ],
+  declarations: [
+    GameDetailComponent
+  ]
+})
+export class StoreFeatureGameDetailModule {}
