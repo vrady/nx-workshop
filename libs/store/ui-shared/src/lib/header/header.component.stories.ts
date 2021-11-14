24dc3e54e10ae08a745a9aba20acd71f2de75c49
vrady <wiggledima@gmail.com>
2021-11-14 21:15:43 +0200

    add storybook

---
+++ libs/store/ui-shared/src/lib/header/header.component.stories.ts
@@ -0,0 +1,27 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
+
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+
+import { HeaderComponent } from './header.component';
+
+export default {
+  title: 'HeaderComponent',
+  component: HeaderComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatToolbarModule],
+    })
+  ],
+} as Meta<HeaderComponent>;
+
+const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
+  component: HeaderComponent,
+  props: args,
+});
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+    title:  '',
+}
